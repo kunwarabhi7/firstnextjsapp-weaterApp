4501bd6564cb2c49121956b76d8078077df7f382
Abhishek <abhishekparmar162@gmail.com>
2022-10-09 23:39:59 +0530

    make mobile responsive

--- Components/Weather.jsx
+++ Components/Weather.jsx
@@ -4,32 +4,42 @@ import React from "react";
 const Weather = ({ weatherData }) => {
   console.log(weatherData);
   return (
-    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto  text-gray-300 z-10">
-      <div className="relative flex flex-col  justify-between pt-12">
-        <div className="flex flex-col items-center">
+    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
+      <div className="relative flex   justify-between pt-12">
+        <div className="flex flex-col ">
           <Image
             alt="/"
             src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-            height="200"
-            width="200"
+            height="100"
+            width="100"
           />
-          <h1 className="text-6xl  ">
-            Temp:{weatherData.main.temp.toFixed(1)}&#8451;
-          </h1>
+          <p className="text-xl ">{weatherData.weather[0].main}</p>
         </div>
-        {/*Bottom */}
-        <div className="flex justify-between ">
-          <div className=" bg-black/50 relative p-2 ml-0 mr-36 mt-16 sm:mr-2 rounded-md ">
-            <h1 className="text-5xl ">
-              Min Temp:{weatherData.main.temp_min.toFixed(1)}&#8451;
-            </h1>
-            <h1 className="text-5xl">
-              Max Temp:{weatherData.main.temp_max.toFixed(1)}&#8451;
-            </h1>
+        <p className="text-8xl md:text-2xl sm:text-2xl">
+          Temp:{weatherData.main.temp.toFixed(1)}&#8451;
+        </p>
+      </div>
+
+      {/*Bottom */}
+      <div className="bg-black/50 relative p-8 rounded-md">
+        <p className="text-2xl text-center pb-6">
+          Weather in {weatherData.name}
+        </p>
+        <div className="flex justify-between text-center">
+          <div>
+          <p className="font-bold text-2xl">{weatherData.main.temp_min.toFixed(1)}&#8451;</p>
+          <p className="text-xl">Min Temp </p>
+            </div>
+
+            <div>
+            <p className="font-bold text-2xl">
+              {weatherData.main.temp_max.toFixed(1)}&#8451;
+            </p>
+            <p className="text-xl">Max Temp</p>
           </div>
-          <div className="bg-black/50 relative p-8 mt-16  rounded-md">
-            <h1 className="text-5xl">Location:{weatherData.name}</h1>
-            <h1 className="text-5xl">Counry:{weatherData.sys.country}</h1>
+          <div>
+              <p className="font-bold text-2xl">{weatherData.sys.country}</p>
+              <p className="text-xl">Counry</p>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,10 @@ module.exports = {
       'sm': {'min': '50px', 'max': '367px'},
       // => @media (min-width: 640px and max-width: 767px) { ... }
 
-      'md': {'min': '368px', 'max': '1023px'},
+      'md': {'min': '368px', 'max': '768px'},
       // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-      'lg': {'min': '1024px', 'max': '1279px'},
+      'lg': {'min': '769', 'max': '1279px'},
       // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
       'xl': {'min': '1280px', 'max': '1535px'},
